f6f4b65f91f0ba1c11f0c48bdd2e25b479cb1bd0
S Mayur <Mayurmamatha9916@gmail.com>
2025-11-06 12:43:35 +0530

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: '/Users/mayurshadhidhar/Music-Mood-Player/client',
-  },
   images: {
     remotePatterns: [
       {
